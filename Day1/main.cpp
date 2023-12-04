df696f2217f5c20e51a22d7315c448c0083516aa
SOBEX <sobexxxxx@gmail.com>
2023-12-04 13:23:53 +0100

    Day 1 - C++ minified

--- Day1/main.cpp
+++ Day1/main.cpp
@@ -1,3 +1,5 @@
+#define MYCHOICE 1
+#if MYCHOICE==0
 #include<iostream>
 #include<fstream>
 #include<string>
@@ -91,3 +93,20 @@ int main(){
    std::cout<<solve2(example2)<<'\n';
    std::cout<<solve2(input)<<'\n';
 }
+#elif MYCHOICE==1
+#include<fstream>
+#include<iostream>
+#include<string>
+#include<vector>
+int main(){
+   std::vector<std::string>digits{"0","zero","1","one","2","two","3","three","4","four","5","five","6","six","7","seven","8","eight","9","nine"};
+   std::ifstream input("input");
+   int sum{0},first{-1},last{-1};
+   for(std::string line;std::getline(input,line);first=last=(sum+=first*10+last)*0-1)
+      for(std::string_view sv=line;!sv.empty();sv.remove_prefix(1))
+         for(int i=0;i<digits.size();i++)
+            if(sv.starts_with(digits[i]))
+               first+=((last=i/2)+1)*(first==-1);
+   std::cout<<sum<<'\n';
+}
+#endif
