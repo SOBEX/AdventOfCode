457c1fc49500dd3e51e7f2c23aad73a02812808c
SOBEX <sobexxxxx@gmail.com>
2023-12-05 20:48:20 +0100

    Day 5 - C++ timing

--- Day5/main.cpp
+++ Day5/main.cpp
@@ -1,11 +1,30 @@
+#include<chrono>
 #include<iostream>
 #include<fstream>
 #include<sstream>
 #include<string>
 #include<vector>
+using t_t=decltype(std::chrono::high_resolution_clock::now());
+t_t getTime(){
+   return std::chrono::high_resolution_clock::now();
+}
+using d_t=std::chrono::nanoseconds;
+double getDurationNanoseconds(t_t start,t_t end){
+   return std::chrono::duration_cast<d_t>(end-start).count()/1.0;
+}
+double getDurationMicroseconds(t_t start,t_t end){
+   return std::chrono::duration_cast<d_t>(end-start).count()/1'000.0;
+}
+double getDurationMilliseconds(t_t start,t_t end){
+   return std::chrono::duration_cast<d_t>(end-start).count()/1'000'000.0;
+}
+double getDurationSeconds(t_t start,t_t end){
+   return std::chrono::duration_cast<d_t>(end-start).count()/1'000'000'000.0;
+}
+using llint=long long int;
+
 #include<limits>
 #include<utility>
-using llint=long long int;
 llint solve1(const std::vector<std::string> &input){
    std::vector<llint>source;
    std::vector<llint>destination;
@@ -183,9 +202,23 @@ int main(){
       input.push_back(line);
    }
 
-   std::cout<<solve1(example1)<<'\n';
-   std::cout<<solve1(input)<<'\n';
+   t_t time0=getTime();
+   auto answer1example=solve1(example1);
+   t_t time1=getTime();
+   auto answer1input=solve1(input);
+   t_t time2=getTime();
+   auto answer2example=solve2(example2);
+   t_t time3=getTime();
+   auto answer2input=solve2(input);
+   t_t time4=getTime();
 
-   std::cout<<solve2(example2)<<'\n';
-   std::cout<<solve2(input)<<'\n';
+   double duration0=getDurationMilliseconds(time0,time1);
+   double duration1=getDurationMilliseconds(time1,time2);
+   double duration2=getDurationMilliseconds(time2,time3);
+   double duration3=getDurationMilliseconds(time3,time4);
+
+   std::cout<<"Example 1 took "<<duration0<<"ms: "<<answer1example<<'\n';
+   std::cout<<"Input   1 took "<<duration1<<"ms: "<<(answer1input)<<'\n';
+   std::cout<<"Example 2 took "<<duration2<<"ms: "<<answer2example<<'\n';
+   std::cout<<"Input   2 took "<<duration3<<"ms: "<<(answer2input)<<'\n';
 }
