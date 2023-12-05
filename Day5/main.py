db48deb31c57774e26dbc5c575af7facb85ba01c
SOBEX <sobexxxxx@gmail.com>
2023-12-05 20:47:10 +0100

    Day 5 - Python timing

--- Day5/main.py
+++ Day5/main.py
@@ -1,3 +1,5 @@
+import time
+
 def doMapping(seed,mapping):
    for row in mapping:
       if row[1]<=seed and seed<row[1]+row[2]:
@@ -36,8 +38,12 @@ def doRangeMappings(seedRanges,mappings):
 def solve2(seeds,mappings):
    return min(doRangeMappings(seeds,mappings),key=lambda seedRange:seedRange[0])[0]
 
+time0=time.perf_counter()
+
 with open('input','r') as file:
    input=file.read().rstrip('\n')
+
+time1=time.perf_counter()
 
 seeds,*mappings=input.split('\n\n')
 seeds=[int(n) for n in seeds.split(':')[1].split()]
@@ -46,5 +52,21 @@ seeds1=sorted(seeds)
 seeds2=sorted([(seed[0],seed[0]+seed[1]) for seed in zip(seeds[::2],seeds[1::2])],key=lambda seed:seed[0])
 mappings=[sorted(mapping,key=lambda row:row[0]) for mapping in mappings]
 
-print(solve1(seeds1,mappings))
-print(solve2(seeds2,mappings))
+time2=time.perf_counter()
+
+answer1=solve1(seeds1,mappings)
+
+time3=time.perf_counter()
+
+answer2=solve2(seeds2,mappings)
+
+time4=time.perf_counter()
+
+read=(time1-time0)*1000
+parsed=(time2-time1)*1000
+part1=(time3-time2)*1000
+part2=(time4-time3)*1000
+print(f'Read input in {read}ms')
+print(f'Parsed input in {parsed}ms')
+print(f'Answer to part 1 is {answer1}, computed in {part1}ms')
+print(f'Answer to part 2 is {answer2}, computed in {part2}ms')
