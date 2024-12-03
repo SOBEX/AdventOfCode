5bd06e02e6606440e53b2521339556421fa30cb1
SOBEX <sobexxxxx@gmail.com>
2024-12-03 07:49:46 +0100

    Template - Python cleanup

--- Template/main.py
+++ Template/main.py
@@ -1,37 +1,45 @@
 import time
 
-#
-
 def solve1():
    return -1
 
 def solve2():
    return -1
 
-time0=time.perf_counter_ns()
+def readsplit(filename):
+   with open(filename,'r') as file:
+      return file.read().rstrip('\n').split('\n')
 
-with open('input','r') as file:
-   input=file.read().rstrip('\n').split('\n')
+def main(filename='input'):
+   time0=time.perf_counter_ns()
 
-time1=time.perf_counter_ns()
+   input=readsplit(filename)
 
-#
+   time1=time.perf_counter_ns()
 
-time2=time.perf_counter_ns()
+   #
 
-answer1=solve1()
+   time2=time.perf_counter_ns()
 
-time3=time.perf_counter_ns()
+   answer1=solve1()
 
-answer2=solve2()
+   time3=time.perf_counter_ns()
 
-time4=time.perf_counter_ns()
+   answer2=solve2()
 
-read=(time1-time0)/1000000.0
-parse=(time2-time1)/1000000.0
-part1=(time3-time2)/1000000.0
-part2=(time4-time3)/1000000.0
-print(f'Read     input  in {read}ms')
-print(f'Parsed   input  in {parse}ms')
-print(f'Answered part 1 in {part1}ms: {answer1}')
-print(f'Answered part 2 in {part2}ms: {answer2}')
+   time4=time.perf_counter_ns()
+
+   read=(time1-time0)/1000000.0
+   parse=(time2-time1)/1000000.0
+   part1=(time3-time2)/1000000.0
+   part2=(time4-time3)/1000000.0
+   print(f'Read     input  in {read}ms')
+   print(f'Parsed   input  in {parse}ms')
+   print(f'Answered part 1 in {part1}ms: {answer1}')
+   print(f'Answered part 2 in {part2}ms: {answer2}')
+
+   return ((answer1,answer2),(read,parse,part1,part2))
+
+if __name__=='__main__':
+   main('example')
+   main()
